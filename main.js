772fd5960cd4cdc58ea4f1489db8b08b2dae4056
amr mersal <amral3117@gmail.com>
2024-12-13 18:02:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+///change nav bar style
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-srcoll', window.scrollY > 0)
+
+
+
+})
+////show //hide faq
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'fa-solid fa-plus') {
+            icon.className = "fa-solid fa-minus";
+        }
+        else {
+            icon.className = "fa-solid fa-plus";
+        }
+    })
+})
+
+// show menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+////close
+const closenav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click',closenav)
